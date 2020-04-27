415f2d8368a5a54fd64eaef5e06efa87870a3801
akine <weezer.and.you@gmail.com>
2020-04-27 09:34:09 +0900

    fix typo url

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 function start() {
   function onRequest(request, response) {
-    var pathname = url.parse(request.rul).pathname;
+    var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
